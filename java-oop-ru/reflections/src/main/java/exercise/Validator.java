cefc4ef0ae32afe8659ee730d4f5c7cc03cfe1b7
@hexlet/cli <support@hexlet.io>
2024-10-24 22:40:49 +0300

    submit java-oop-ru/reflections

--- java-oop-ru/reflections/src/main/java/exercise/Validator.java
+++ java-oop-ru/reflections/src/main/java/exercise/Validator.java
@@ -2,29 +2,24 @@ package exercise;
 
 import java.lang.reflect.Field;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
-import java.util.stream.Collectors;
 
 // BEGIN
 class Validator {
-    public static List<String> validate(Object instance) {
-        List<Field> fields = List.of(instance.getClass().getDeclaredFields());
-        return fields.stream()
-                .filter(field -> field.isAnnotationPresent(NotNull.class))
-                .filter(field -> {
-                    Object value;
-                    try {
-                        field.setAccessible(true);
-                        value = field.get(instance);
-//                        field.setAccessible(false);
-                    } catch (Exception e) {
-                        throw new RuntimeException(e);
-                    }
-                    return value == null;
-                })
-                .map(Field::getName)
-                .collect(Collectors.toList());
+    public static List<String> validate(Address address) {
+        List<String> result = new ArrayList<>();
+        Field[] fields = address.getClass().getDeclaredFields();
+        try {
+            for(var field : fields) {
+                field.setAccessible(true);
+                if (field.isAnnotationPresent(NotNull.class) && field.get(address) == null) {
+                    result.add(field.getName());
+                }
+            }
+        } catch (IllegalAccessException e) {
+            e.printStackTrace();
+        }
+        return result;
     }
 }
 // END
